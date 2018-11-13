0540f5221335af5d962dd685a121b4ac54e012fc
Daniel Scanland <daniel.scanland@gmail.com>
2018-11-13 13:53:48 -0700

    commit 1 and 2

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Console.js
@@ -0,0 +1,4 @@
+console.log(33);
+console.log(8);
+
+<!-- I learned that the console is where important messages are displayed and the console keyword refers to the collection of data -->
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/What-is-JavaScript.js
@@ -0,0 +1,1 @@
+<!-- I learned that javascript is the language of most modern web browsers and is considered the core of web development -->
