aa6453ea7ec4b37e5e2c3091d60b48baf6d377a8
Daniel Scanland <daniel.scanland@gmail.com>
2018-11-14 11:08:56 -0700

    commit 7-10

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,8 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// I learned truthy and falsy assignments or short circuit evaluation to create a short hand way from evaluating non-boolean if...else statements
