ea6ea90948f0ec1ca7e08024b8d1f7efa5fde658
Daniel Scanland <daniel.scanland@gmail.com>
2018-11-15 06:25:21 -0700

    commit loops 8 & 9

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,10 @@
+// Write your code below
+
+const cupsOfSugarNeeded = 8;
+const cupsAdded = 0;
+
+do {
+  cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);
+
+// I learned how to use a do...while loop and that a do...while can still run at least the first line of code even if the statement is false
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-break-keyword.js
@@ -0,0 +1,15 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write you code below
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] ===  'Notorious B.I.G.'){
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.")
+
+
+
+// I learned how to add breaks in for loops so that the loop can stop once a certain condition is met and not run until the end of the defined loop terms
